96b4ffbb781f0a12df274f9e3ddede0e03e70452
Dias <nurgalyev.dias@gmail.com>
2023-05-31 00:00:05 +0600

    Initialization

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textfield2: UITextField!
+    @IBOutlet weak var textfield1: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func plus(_ sender: Any) {
+        let a = textfield1.text!
+        let b = textfield2.text!
+        let result = Int(a)! + Int(b)!
+        label.text = "Result is: \(result)"
+    }
+    @IBAction func minus(_ sender: Any) {
+        let a = textfield1.text!
+        let b = textfield2.text!
+        let result = Int(a)! - Int(b)!
+        label.text = "Result is: \(result)"
+    }
+    @IBAction func multiply(_ sender: Any) {
+        let a = textfield1.text!
+        let b = textfield2.text!
+        let result = Int(a)! * Int(b)!
+        label.text = "Result is: \(result)"
+    }
+    
+    @IBAction func divide(_ sender: Any) {
+        let a = textfield1.text!
+        let b = textfield2.text!
+        let result = Double(a)! / Double(b)!
+        label.text = "Result is: \(result)"
+    }
 }
 
